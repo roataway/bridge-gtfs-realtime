da5f1e94f21d4d18bd00124cb26b09cf6fa39b6c
Alexander Railean <alexandr.railean@arculus.de>
2021-04-08 00:46:34 +0200

    Make black, flake8 and bandit happy

--- structures.py
+++ structures.py
@@ -1,4 +1,3 @@
-import json
 from datetime import datetime
 
 from util import random_word
@@ -6,6 +5,7 @@ from util import random_word
 
 class FeedEntity:
     """A class to represent the state of a vehicle, ready for serialization to ProtoBuf"""
+
     id: str
     vehicle_id: str
     vehicle_label: str
@@ -18,15 +18,17 @@ class FeedEntity:
 
     def __init__(self, data: dict, route_id: str):
         self.id = random_word(6)
-        self.vehicle_id = data['board']
-        self.vehicle_label = 'troleibuz'
-        datetime_object = datetime.strptime(data['timestamp'], '%Y-%m-%dT%H:%M:%SZ')
+        self.vehicle_id = data["board"]
+        self.vehicle_label = "troleibuz"
+        datetime_object = datetime.strptime(data["timestamp"], "%Y-%m-%dT%H:%M:%SZ")
         self.vehicle_timestamp = datetime_object.timestamp()  # this must be the time when the server received it?
-        self.vehicle_position_lat = data['latitude']
-        self.vehicle_position_lot = data['longitude']
-        self.vehicle_speed = data['speed'] / 3.6  # convert to m/s
+        self.vehicle_position_lat = data["latitude"]
+        self.vehicle_position_lot = data["longitude"]
+        self.vehicle_speed = data["speed"] / 3.6  # convert to m/s
         self.vehicle_trip_id = route_id
 
     def __str__(self):
-        return f'lat:{self.vehicle_position_lat}, lon:{self.vehicle_position_lot}, route_id:{self.vehicle_trip_id} ' \
-               f'@{datetime.fromtimestamp(self.vehicle_timestamp)}Z'
+        return (
+            f"lat:{self.vehicle_position_lat}, lon:{self.vehicle_position_lot}, route_id:{self.vehicle_trip_id} "
+            f"@{datetime.fromtimestamp(self.vehicle_timestamp)}Z"
+        )
